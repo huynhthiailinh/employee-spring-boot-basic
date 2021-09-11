51e2ff3b84618ca6b226d4064e80a48059fa6088
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2021-09-11 08:13:08 +0700

    feature: upload image

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,12 +1,23 @@
 package com.example.employee.repository;
 
 import com.example.employee.model.Employee;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
     List<Employee> findEmployeesByName(String name);
+
+    @Modifying
+    @Query("UPDATE Employee e SET e.avatar = :avatar WHERE e.id = :id")
+    void updateAvatarById(@Param("id") int id, @Param("avatar") String avatar);
+
 }
